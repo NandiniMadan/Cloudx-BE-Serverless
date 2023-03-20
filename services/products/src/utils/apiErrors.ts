bcdddaad8adfa54ca3af2df391a857906a32f4a5
Nandini Madan <34620886+NandiniMadan@users.noreply.github.com>
2023-03-20 21:06:21 +0530

    task 4 complete

---
+++ services/products/src/utils/apiErrors.ts
@@ -0,0 +1,20 @@
+export class ApiError {
+    statusCode: number;
+    body: string;
+    headers: {
+        'Content-Type': string;
+        'Access-Control-Allow-Origin': string;
+    };
+    constructor(
+        message = 'An error occurred',
+        statusCode = 500,
+    ) {
+        const body = JSON.stringify({ message });
+        this.statusCode = statusCode;
+        this.body = body;
+        this.headers = {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        };
+    }
+}
